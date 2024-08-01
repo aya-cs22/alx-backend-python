abe22cb5471add21298f1c9f5c55fdf0d95d0db8
aya-cs22 <aya786930@gmail.com>
2024-08-01 13:55:29 +0300

    Complex types - mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[float, int]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
     total_list = 0
     for num in mxd_lst:
         total_list += num
