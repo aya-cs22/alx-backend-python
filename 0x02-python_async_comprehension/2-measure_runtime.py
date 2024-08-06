5b2b365cf8c110520db9029634c233e07a41a657
aya-cs22 <aya786930@gmail.com>
2024-08-06 14:04:44 +0300

    Run time for four parallel comprehensions

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
-
+"""Mesures time"""
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime():
-    start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end_time = time.time()
-    return end_time - start_time
 
+async def measure_runtime() -> float:
+    """Executes async_comprehension four times in parallel
+        and measure_runtime should measure the total runtime and return it"""
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    return time.perf_counter() - start_time
 # #!/usr/bin/env python3
 # """Run time for four parallel comprehensionse"""
 # import asyncio
