63a15891754f30cff57d6ff247430104786fd4b2
aya-cs22 <aya786930@gmail.com>
2024-08-06 14:29:51 +0300

    Run time for four parallel comprehensions

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,33 +1,17 @@
 #!/usr/bin/env python3
-"""Mesures time"""
+"""Run time for four parallel comprehensionse"""
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Executes async_comprehension four times in parallel
-        and measure_runtime should measure the total runtime and return it"""
+    """execute async_comprehension 4 times in parallel using asyncio.gather."""
     start_time = time.perf_counter()
-    await asyncio.gather(async_comprehension(), async_comprehension(),
-                         async_comprehension(), async_comprehension())
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
     return time.perf_counter() - start_time
-# #!/usr/bin/env python3
-# """Run time for four parallel comprehensionse"""
-# import asyncio
-# import time
-# async_comprehension = __import__('1-async_comprehension').async_comprehension
-
-
-# async def measure_runtime() -> float:
-#     """execute async_comprehension 4 times in parallel using asyncio.gather."""
-#     start_time = time.perf_counter()
-#     await asyncio.gather(
-#         async_comprehension(),
-#         async_comprehension(),
-#         async_comprehension(),
-#         async_comprehension(),
-#     )
-#     end_time = time.perf_counter()
-#     total_time = end_time - start_time
-#     return total_time
